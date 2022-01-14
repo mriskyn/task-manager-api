aa9f09f052f263a71e780440852a9422dae7a005
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-14 22:03:56 +0700

    added optional router

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,4 +8,7 @@ router.get('/:id', getTask)
 router.patch('/:id', updateTask)
 router.delete('/:id', deleteTask)
 
+// router.route('/').get(getAllTasks).post(createTask)
+// router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
 module.exports = router;
